340ee7d78e2e43674b9e9f86cbcda112c5c957a0
bkalika <bogdan.kalika@gmail.com>
2022-01-14 19:38:02 +0200

    2.6 JPA Entities

---
+++ src/main/java/com/udemy/udemyspringframework5beginnertoguru/UdemySpringFramework5BeginnerToGuruApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.udemyspringframework5beginnertoguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringFramework5BeginnerToGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySpringFramework5BeginnerToGuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udemy/udemyspringframework5beginnertoguru/UdemySpringFramework5BeginnerToGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.udemyspringframework5beginnertoguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringFramework5BeginnerToGuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
