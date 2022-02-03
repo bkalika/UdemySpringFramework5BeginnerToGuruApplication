425a5d9d002df7debe957007d40ae4c196f80130
bkalika <bogdan.kalika@gmail.com>
2022-02-04 01:27:26 +0200

    DI, @Autowired, @Service, @Controller, @Qualifier

---
+++ src/main/java/com/udemy/udemyspringframework5beginnertoguru/di/src/test/java/guru/springframework/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.udemyspringframework5beginnertoguru.di.src.test.java.guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
